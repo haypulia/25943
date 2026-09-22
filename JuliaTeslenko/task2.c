cac60e89cb3d0b40042333aa1d9064f635e37144
haypulia <julieteslenko@yandex.ru>
2026-09-22 19:13:21 +0700

    Update task2.c

--- JuliaTeslenko/task2.c
+++ JuliaTeslenko/task2.c
@@ -17,6 +17,13 @@ int main(void)
     }
 
     printf("%s", asctime(tm));
+    printf("%d/%d/%02d %02d:%02d:%02d PST\n",
+           tm->tm_mon + 1,
+           tm->tm_mday,
+           tm->tm_year % 100,
+           tm->tm_hour,
+           tm->tm_min,
+           tm->tm_sec);
 
     return 0;
 }
